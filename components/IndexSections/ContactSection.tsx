f4f60624d6c7f4ca702cdeab5cf4ea44afb6744c
Sebastian <24754485+mildlyadequate@users.noreply.github.com>
2022-03-29 12:53:37 +0200

    temporary test form

--- components/IndexSections/ContactSection.tsx
+++ components/IndexSections/ContactSection.tsx
@@ -43,7 +43,29 @@ const ContactSection = () => {
 
             <div className='flex w-full'>
 
-                <form name="contact-form" method="POST" className={"block w-full md:w-3/4 mr-8"} data-netlify="true">
+                <form name="contact" method="POST" data-netlify="true">
+                    <p>
+                        <label>Your Name: <input type="text" name="name" /></label>
+                    </p>
+                    <p>
+                        <label>Your Email: <input type="email" name="email" /></label>
+                    </p>
+                    <p>
+                        <label>Your Role: <select name="role[]" multiple>
+                            <option value="leader">Leader</option>
+                            <option value="follower">Follower</option>
+                        </select></label>
+                    </p>
+                    <p>
+                        <label>Message: <textarea name="message"></textarea></label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
+
+
+                {/* <form name="contact-form" method="POST" className={"block w-full md:w-3/4 mr-8"} data-netlify="true">
                     <div className='flex flex-col'>
                         <label
                             htmlFor="name"
@@ -93,7 +115,7 @@ const ContactSection = () => {
                     >
                         Send
                     </button>
-                </form>
+                </form> */}
 
             </div>
         </div>
